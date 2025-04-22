6f6f4368031724fe80028b48b46312b71516d585
Pavan Kumar Vinjamuri <pavanvinjamuri212@gmail.com>
2025-04-21 22:56:11 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,11 @@ set -e
 RELEASE_NAME=$1
 IMAGE_TAG=$2
 
+if [ -z "$IMAGE_TAG" ]; then
+  echo "ERROR: IMAGE_TAG is empty. Aborting Helm deploy."
+  exit 1
+fi
+
 helm upgrade --install $RELEASE_NAME ./helm-chart \
   --namespace monitoring \
   --create-namespace \
