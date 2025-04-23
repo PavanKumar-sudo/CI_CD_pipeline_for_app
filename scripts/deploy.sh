1bebc33214765ed5bd7c92451587efcee1bddd5f
Pavan Kumar Vinjamuri <pavanvinjamuri212@gmail.com>
2025-04-23 15:58:19 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,17 +4,12 @@ set -e
 
 RELEASE_NAME=$1
 IMAGE_TAG=$2
-DOMAIN=$3
 
 if [ -z "$IMAGE_TAG" ]; then
   echo "ERROR: IMAGE_TAG is empty. Aborting Helm deploy."
   exit 1
 fi
 
-if [ -z "$DOMAIN" ]; then
-  echo "ERROR: DOMAIN is empty. Aborting Helm deploy."
-  exit 1
-fi
 
 helm upgrade --install $RELEASE_NAME ./helm-chart \
   --namespace monitoring \
@@ -23,5 +18,5 @@ helm upgrade --install $RELEASE_NAME ./helm-chart \
   --set backend.sessionSecret="${SESSION_SECRET}" \
   --set smtpAuth.username="${SMTP_USERNAME}" \
   --set smtpAuth.password="${SMTP_PASSWORD}" \
-  --set global.domain="${DOMAIN}" \
+  --set global.domain="go-web-app.local" \
   --wait
